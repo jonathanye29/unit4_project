f659e75e7d8ea430512a783b615e83c1ebd11e4e
Jonathanye29 <111751273+jonathanye29@users.noreply.github.com>
2023-05-07 14:45:54 +0900

    Update my_lib.py

--- my_lib.py
+++ my_lib.py
@@ -16,6 +16,11 @@ class database_worker:
     def close(self):
         self.connection.close()
 
+    def run_fetchone(self, query):
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        return result
+
 #code for the password hashing
 from passlib.hash import sha256_crypt
 
